96901dde43bc6be055cb6d3091bf09cb89ef2e03
Hanuvesh Singh <hanuveshsingh2506@gmail.com>
2025-02-24 16:00:25 +0530

    added Models

---
+++ src/main/java/models/Cart.java
@@ -0,0 +1,39 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity(name="CART")
+public class Cart
+{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name="customer_id")
+    private User customer;
+
+    public Cart() {
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public User getCustomer()
+    {
+        return customer;
+    }
+
+    public void setCustomer(User customer)
+    {
+        this.customer = customer;
+    }
+}
